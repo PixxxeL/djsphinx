c3855e4950b2641fa46197e0c423441b0d4ba047
pixxxel <ivan.n.sergeev@gmail.com>
2021-12-02 11:49:34 +0500

    Fix link on license

--- djsphinx/__init__.py
+++ djsphinx/__init__.py
@@ -3,5 +3,5 @@ try:
 except ImportError:
     pass
 
-VERSION = (0, 0, 10,)
+VERSION = (0, 0, 11,)
 __version__ = '.'.join(map(str, VERSION))
--- setup.py
+++ setup.py
@@ -10,15 +10,11 @@ setup(
     author_email = 'ivan.n.sergeev@gmail.com',
     maintainer = 'pixel',
     maintainer_email = 'ivan.n.sergeev@gmail.com',
-    license = 'GPL3',
+    license = 'MIT',
     description = 'Sphinx Search Engine integration with Django ORM through Python client for Sphinx by Michael Kurze, James Socol',
     download_url = 'https://github.com/PixxxeL/djsphinx/archive/master.zip',
     classifiers = [
         'Operating System :: OS Independent',
         'Programming Language :: Python',
     ],
-    #include_package_data = True,
-    #install_requires = [
-    #    'https://github.com/jsocol/sphinxapi/archive/master.zip'
-    #],
 )
